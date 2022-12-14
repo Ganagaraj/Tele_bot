b4db2c28dc3b2a37ca16ba0c944cc22a178cb39a
Ganagaraj <rajdoescode@gmail.com>
2022-12-14 18:41:58 +0530

    missed bot in index.js

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ app.get("/",(req,res)=>{
 app.post("/new-message", (request, response) => {
     const { message } = request.body;
     if (message.toLowerCase() == 'ganagaraj') {
-        axios.post("https://api.telegram.org/5880824097:AAFm5kqNB_2gv3993pDSN5V6alXnKN4hbSU/sendMessage", {
+        axios.post("https://api.telegram.org/bot5880824097:AAFm5kqNB_2gv3993pDSN5V6alXnKN4hbSU/sendMessage", {
             chat_id: message.chat.id,
             text: "well he is my boss",
         }).then((res) => {
             console.log(res + " from tele bot");
+            
         }).catch((err) => {
             console.log(err);  
         })
