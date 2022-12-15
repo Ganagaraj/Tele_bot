150239f3378f8f20059dc587703011a121bbac1b
Ganagaraj <rajdoescode@gmail.com>
2022-12-15 10:45:35 +0530

    changed the functionality in index.js

--- index.js
+++ index.js
@@ -7,22 +7,29 @@ app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({ extended: true })
 );
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.end("<h1>Telegram bot</h1>")
 })
 
+function send(id, msg) {
+    axios.post("https://api.telegram.org/bot5880824097:AAFm5kqNB_2gv3993pDSN5V6alXnKN4hbSU/sendMessage", {
+        chat_id: id,
+        text: msg
+    }).then((res) => {
+        console.log(res + " from tele bot");
+        response.end('done')
+    }).catch((err) => {
+        console.log(err);
+        response.end(err);
+    })
+}
 app.post("/", (request, response) => {
     const { message } = request.body;
-        axios.post("https://api.telegram.org/bot5880824097:AAFm5kqNB_2gv3993pDSN5V6alXnKN4hbSU/sendMessage", {
-            chat_id: message.chat.id,
-            text: "well he is my boss"
-        }).then((res) => {
-            console.log(res + " from tele bot");
-            response.end('done')
-        }).catch((err) => {
-            console.log(err);  
-            response.end(err);
-        })
+    if (message.text.toLowerCase() == 'ganagaraj') {
+        send(message.chat.id, "well,he is boss")
+    } else {
+        send(message.chat.id, "illa puriyala")
+    }
 
 })
 
