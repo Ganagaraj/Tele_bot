8e0958ee5cf6136565d9bc120dfd739f83eb916d
Ganagaraj <rajdoescode@gmail.com>
2022-12-15 10:56:04 +0530

    final

--- index.js
+++ index.js
@@ -11,24 +11,31 @@ app.get("/", (req, res) => {
     res.end("<h1>Telegram bot</h1>")
 })
 
-function send(id, msg) {
-    axios.post("https://api.telegram.org/bot5880824097:AAFm5kqNB_2gv3993pDSN5V6alXnKN4hbSU/sendMessage", {
-        chat_id: id,
-        text: msg
-    }).then((res) => {
-        console.log(res + " from tele bot");
-        response.end('done')
-    }).catch((err) => {
-        console.log(err);
-        response.end(err);
-    })
-}
+
 app.post("/", (request, response) => {
     const { message } = request.body;
     if (message.text.toLowerCase() == 'ganagaraj') {
-        send(message.chat.id, "well,he is boss")
+        axios.post("https://api.telegram.org/bot5880824097:AAFm5kqNB_2gv3993pDSN5V6alXnKN4hbSU/sendMessage", {
+            chat_id: message.chat.id,
+            text: "well he is my boss"
+        }).then((res) => {
+            console.log(res + " from tele bot");
+            response.end('done')
+        }).catch((err) => {
+            console.log(err);
+            response.end(err);
+        })
     } else {
-        send(message.chat.id, "illa puriyala")
+        axios.post("https://api.telegram.org/bot5880824097:AAFm5kqNB_2gv3993pDSN5V6alXnKN4hbSU/sendMessage", {
+            chat_id: message.chat.id,
+            text: "illa puriyala"
+        }).then((res) => {
+            console.log(res + " from tele bot");
+            response.end('done')
+        }).catch((err) => {
+            console.log(err);
+            response.end(err);
+        })
     }
 
 })
